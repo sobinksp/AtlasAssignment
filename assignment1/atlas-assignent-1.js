fba1d3f6327af7ef8b463555ac453a8d00f6f7ae
Songphon Chamniansong <songphon.cha@ondemand.in.th>
2023-08-10 13:57:04 +0700

    add assignment

---
+++ assignment1/atlas-assignent-1.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @param input
+ * {ให้เขียนฟังก์ชั่นที่รับข้อมูลเป็น Array ของ number จากนั้น Return เป็น Array ที่ถูก +1 
+    * ตัวอย่าง
+    * input [2,3] return [2,3]
+    * input [9] return [1,0]
+    * input [9,9,9] return [1,0,0,0]
+    * input [7,8,8] return [7,8,9]}  
+ */
+const atlasAssignment1 = (input) => {
+
+}
