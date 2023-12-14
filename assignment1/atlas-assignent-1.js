2c83b696b9861e9387b4a8366fec4910ab41d0b5
Sobin <sobingeneral@gmail.com>
2023-12-14 12:06:39 +0700

    updated assignment

--- assignment1/atlas-assignent-1.js
+++ assignment1/atlas-assignent-1.js
@@ -1,13 +1,21 @@
 /**
- * 
+ *
  * @param input
- * {ให้เขียนฟังก์ชั่นที่รับข้อมูลเป็น Array ของ number จากนั้น Return เป็น Array ที่ถูก +1 
-    * ตัวอย่าง
-    * input [2,3] return [2,4]
-    * input [9] return [1,0]
-    * input [9,9,9] return [1,0,0,0]
-    * input [7,8,8] return [7,8,9]}  
+ * {ให้เขียนฟังก์ชั่นที่รับข้อมูลเป็น Array ของ number จากนั้น Return เป็น Array ที่ถูก +1
+ * ตัวอย่าง
+ * input [2,3] return [2,4]
+ * input [9] return [1,0]
+ * input [9,9,9] return [1,0,0,0]
+ * input [7,8,8] return [7,8,9]}
  */
 const atlasAssignment1 = (input) => {
+  const num = parseInt(input.join(""), 10);
+  const numPlusOne = num + 1;
+  const result = Array.from(String(numPlusOne), Number);
+  return result;
+};
 
-}
+console.log(atlasAssignment1([2, 3]));
+console.log(atlasAssignment1([9]));
+console.log(atlasAssignment1([9, 9, 9]));
+console.log(atlasAssignment1([7, 8, 8]));
--- assignment2/src/app.service.ts
+++ assignment2/src/app.service.ts
@@ -5,4 +5,20 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  async getWeather(postCode: String) {
+    const API_KEY = '65c30b3194d7c141789fc9f4789c59e2';
+    const geoRes = await fetch(
+      `http://api.openweathermap.org/geo/1.0/zip?zip=${postCode},TH&appid=${API_KEY}`,
+    );
+
+    const geoData = await geoRes.json();
+    const { lat, lon } = geoData;
+    const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    console.log(weatherApiUrl);
+    console.log(lat, lon);
+    const res = await fetch(weatherApiUrl);
+    const result = res.json();
+    return result;
+  }
 }
